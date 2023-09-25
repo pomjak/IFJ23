3fbb61bec772d5e6aa8e0788e41f01296ef13b08
Adnrew <ado.ponechal@gmail.com>
2023-09-25 20:03:50 +0200

    define error codes

--- src/error.h
+++ src/error.h
@@ -1,9 +1,15 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-/**
- * TODO:
- * define error codes
- */
+#define ERR_LEXIKAL 1
+#define ERR_SYNTAX 2
+#define ERR_UNDEFINED_FUNCTION 3
+#define ERR_FUNCTION_PARAMETER 4  //wrong function parameter type, number of prameter, wrong return type
+#define ERR_UNDEFINED_VARIABLE 5
+#define ERR_FUNCTION_RETURN 6 // missing/useless  expression
+#define ERR_UNCOMPATIBILE_TYPE 7
+#define ERR_MISSING_TYPE 8
+#define ERR_SEMANTIC_ERR 9
+#define ERR_INTERNAL 99
 
 #endif
