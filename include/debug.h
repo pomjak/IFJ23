65990d4e7d64715e5764349f6a544d0db10cf51a
Marie Koláríková <xkolar77@stud.fit.vutbr.cz>
2023-10-06 22:08:46 +0200

    add macro unused

--- include/debug.h
+++ include/debug.h
@@ -1,4 +1,13 @@
+/**
+ * @file lexical_analyzer.h
+ * @brief Lexical analyzer header
+ * @author Marie Kolarikova <xkolar77@stud.fit.vutbr.cz>
+ * @date 06.10.2023
+ **/
+
 #include <stdio.h>
+
+#define UNUSED(x) (void)(x)
 
 #if defined(DEBUG)
     #define DEBUG_PRINT(fmt, ...) fprintf(stderr, "[DEBUG] %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
