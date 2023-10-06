ae44e318482fcd34bcad59f7107bba134daca164
Marie Koláríková <xkolar77@stud.fit.vutbr.cz>
2023-10-06 22:11:09 +0200

    test for lexical analyzer

---
+++ tests/unit/lexical_analyzer_test1/main.c
@@ -0,0 +1,22 @@
+/**
+ * @file main.cz
+ * @brief main to run lexical analyzer
+ * @author Marie Kolarikova <xkolar77@stud.fit.vutbr.cz>
+ * @date 06.10.2023
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lexical_analyzer.h"
+
+int main(int argc, char ** argv) {
+    
+    token_T token;
+
+    do {
+        if (get_token(&token)) return 1;
+        print_token(token);
+    } while (token.type != TOKEN_EOF);
+    
+    return 0;
+}
