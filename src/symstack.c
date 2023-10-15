c61234ee1cee4cf9a900be8bde576a8690b3d6ce
Adnrew <ado.ponechal@gmail.com>
2023-10-15 10:57:45 +0200

    improve previous commit

--- src/symstack.c
+++ src/symstack.c
@@ -1,20 +1,110 @@
+/**
+ * @file symstack.c
+ * @author Adrián Ponechal (xponec01@stud.fit.vutbr.cz)
+ * @brief Stack for symbols used in precedence analysis
+ * @date 2023-10-15
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "symstack.h"
 
-void hello_stack(void)
+void symstack_init_stack(symstack_t *stack)
 {
-    printf("if you see this, you haven't implemented stack...\n");
+    stack->top = NULL;
+    stack->size = 0;
 }
 
-
-#ifdef SYMSTACK_TEST
-
-int main(int argc, char ** argv)
+// push
+bool symstack_push(symstack_t *stack, data_t data)
 {
-    (void)argc;
-    (void)argv;
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    if (new_node == NULL)
+    {
+        return false;
+    }
 
-    return EXIT_SUCCESS;
+    new_node->data = data;
+    new_node->previous = stack->top;
+    stack->top = new_node;
+    stack->size += 1;
+    return true;
 }
 
-#endif
+// pop
+data_t symstack_pop(symstack_t *stack)
+{
+    data_t old_node_data;
+    if (!isEmpty(stack))
+    {
+        node_t *old_node = stack->top;
+        old_node_data = old_node->data;
+
+        stack->top = old_node->previous;
+        stack->size -= 1;
+        free(old_node);
+        return old_node_data;
+    }
+    return old_node_data;
+}
+
+// peek
+data_t symstack_peek(const symstack_t *stack)
+{
+    return stack->top->data;
+}
+
+bool symstack_isEmpty(const symstack_t *stack)
+{
+    return stack->size == 0 && stack->top == NULL;
+}
+
+// dispose
+bool symstack_dispose(symstack_t *stack)
+{
+    return false;
+}
+
+void print_line(unsigned int width)
+{
+    printf("+");
+    for (int i = 1; i < width - 1; i++)
+    {
+        printf("-");
+    }
+    printf("+\n");
+}
+
+void print_node(node_t *node, unsigned int width)
+{
+    print_line(width);
+    printf("| ");
+    for (int i = 2; i < width - 2; i++)
+    {
+        if (i == (int)(width / 2))
+            printf("%c", node->data.c);
+        else
+            printf(" ");
+    }
+    printf(" |\n");
+}
+
+void print_stack(const symstack_t *stack, unsigned int width)
+{
+    node_t nodes[stack->size];
+
+    node_t *current_node = stack->top;
+    for (int i = 0; i < stack->size; i++)
+    {
+        nodes[i] = *current_node;
+        current_node = current_node->previous;
+    }
+
+    for (int i = 0; i < stack->size; i++)
+    {
+        print_node(&nodes[i], width);
+    }
+
+    // print end
+    print_line(width);
+}
