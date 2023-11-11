9508a2abc9d03b5b7a4ac0468d8e24a755b9031e
Simon Cagala <xcagal00@stud.fit.vutbr.cz>
2023-11-11 18:05:17 +0100

    forgot to save

--- include/error.h
+++ include/error.h
@@ -14,13 +14,13 @@
 #define ERR_LEXICAL 1
 #define ERR_SYNTAX 2
 #define ERR_UNDEFINED_FUNCTION 3
-#define ERR_VAR_REDEFINE 3
+#define ERR_REDEFINING_VARIABLE 3
 #define ERR_FUNCTION_PARAMETER 4  //wrong function parameter type, number of prameter, wrong return type
 #define ERR_UNDEFINED_VARIABLE 5
 #define ERR_FUNCTION_RETURN 6 // missing/useless  expression
 #define ERR_UNCOMPATIBILE_TYPE 7
 #define ERR_MISSING_TYPE 8
-#define ERR_SEMANTIC_ERR 9
+#define ERR_SEMANTIC 9
 #define ERR_INTERNAL 99
 
 /**
