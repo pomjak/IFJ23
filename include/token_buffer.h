a2da4d14bc8ca8948869990049a335506cb13880
Simon Cagala <xcagal00@stud.fit.vutbr.cz>
2023-11-14 23:49:06 +0100

    added function declarations to token buffer

--- include/token_buffer.h
+++ include/token_buffer.h
@@ -3,11 +3,56 @@
  * @author Simon Cagala (xcagal00@stud.fit.vutbr.cz)
  * @brief Linked list token buffer header file
  * @date 2023-11-14
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #ifndef TOKEN_BUFFER_H
 #define TOKEN_BUFFER_H
+
+#include "lexical_analyzer.h"
+
+typedef struct token_buffer_node {
+    token_T token;
+    struct token_buffer_node* next;
+} *token_buffer_t;
+
+
+/**
+ * @brief 
+ * @param head Last element in buffer
+ */
+void token_buffer_init(token_buffer_t* head);
+
+/**
+ * @brief Add a new token to buffer
+ * 
+ * @param head Last element in buffer
+ * @param token Added token
+ */
+void token_buffer_add(token_buffer_t* head, token_T token);
+
+/**
+ * @brief Remove last element from buffer
+ * 
+ * @param head Last element in buffer
+ */
+void token_buffer_pop(token_buffer_t* head);
+
+/**
+ * @brief Dispose of the contents in buffer
+ * 
+ * @param head Last element in buffer
+ */
+void token_buffer_dispose(token_buffer_t* head);
+
+/**
+ * @brief Check if buffer is safe to use (not empty) 
+ *
+ * @param head 
+ * @return true if not empty
+ * @return false if empty
+ */
+bool token_buffer_peek(token_buffer_t head);
 
 #endif
--- src/token_buffer.c
+++ src/token_buffer.c
@@ -3,9 +3,29 @@
  * @author Simon Cagala (xcagal00@stud.fit.vutbr.cz)
  * @brief Linked list buffer for loading tokens
  * @date 2023-11-14
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include "token_buffer.h"
+
+void token_buffer_init(token_buffer_t* head) {
+    *head = NULL;
+}
+
+void token_buffer_add(token_buffer_t* head, token_T token) {
+
+}
+
+void token_buffer_pop(token_buffer_t* head) {
+
+}
+
+void token_buffer_dispose(token_buffer_t* head) {
+    
+}
+
+bool token_buffer_peek(token_buffer_t head) {
+    return (head != NULL);
+}
