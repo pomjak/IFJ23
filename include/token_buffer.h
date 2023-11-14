869e4e9c262b545d84422faaa9e249b865a9c86d
Simon Cagala <xcagal00@stud.fit.vutbr.cz>
2023-11-14 23:22:32 +0100

    created token_buffer files

---
+++ include/token_buffer.h
@@ -0,0 +1,13 @@
+/**
+ * @file token_buffer.h
+ * @author Simon Cagala (xcagal00@stud.fit.vutbr.cz)
+ * @brief Linked list token buffer header file
+ * @date 2023-11-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef TOKEN_BUFFER_H
+#define TOKEN_BUFFER_H
+
+#endif
---
+++ src/token_buffer.c
@@ -0,0 +1,11 @@
+/**
+ * @file token_buffer.c
+ * @author Simon Cagala (xcagal00@stud.fit.vutbr.cz)
+ * @brief Linked list buffer for loading tokens
+ * @date 2023-11-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "token_buffer.h"
