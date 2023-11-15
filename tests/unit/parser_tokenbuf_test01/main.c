730ec946cc2c87d3d5ce92bf0b30952914ab84c9
Simon Cagala <xcagal00@stud.fit.vutbr.cz>
2023-11-15 14:23:10 +0100

    updated token buffer test01

--- tests/unit/parser_tokenbuf_test01/main.c
+++ tests/unit/parser_tokenbuf_test01/main.c
@@ -7,6 +7,8 @@
  */
 
 #include "token_buffer.h"
+#include "lexical_analyzer.h"
+#include "error.h"
 #include <assert.h>
 
 int main()
@@ -14,11 +16,23 @@ int main()
     token_buffer_t buffer;
     tb_init(&buffer);
     token_T token;
+    token_T tmp;
     int result;
-    result = get_token(&token);
-    assert(result == 0);
-    assert(tb_push(&buffer, token) == 0);
+
+    do {
+        if (get_token(&token)) {
+            tb_dispose(&buffer);
+            return 1;
+        }
+        tb_push(&buffer, token);
+        tmp = tb_get_token(&buffer);
+        print_token(tmp);
+        if(tmp.type == TOKEN_STRING || tmp.type == TOKEN_IDENTIFIER) {
+            dstring_free(&tmp.value.string_val);
+        }
+    } while (token.type != TOKEN_EOF);
+
     tb_dispose(&buffer);
-    
+
     return 0;
 }
