9801e126c4619b3f2f4b507c2ca114edbd56838b
pomjak <pomjak71@gmail.com>
2023-11-17 15:37:27 +0100

    add parser test 8: call builtins

---
+++ tests/unit/parser_test08/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * @author Jakub Pomsar (xpomsa00@stud.fit.vutbr.cz)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <assert.h>
+#include "parser.h"
+
+int main()
+{
+    uint32_t res;
+    res = parse();
+    // fprintf(stderr, "res: %d", res);
+    return res;
+}
