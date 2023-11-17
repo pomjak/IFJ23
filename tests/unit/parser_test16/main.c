dedf63bb9bdcb033d68f2da19d06c94838a9d602
pomjak <pomjak71@gmail.com>
2023-11-18 00:54:33 +0100

    added test 16 return missing expt

---
+++ tests/unit/parser_test16/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author Jakub Pomsar (xpomsa00@stud.fit.vutbr.cz)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <assert.h>
+#include "parser.h"
+
+int main()
+{
+    assert(parse() == 6);
+    return 0;
+}
