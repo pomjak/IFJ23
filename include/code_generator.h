69704a170600492a35286716eef2a4463d9c94b0
Marie Koláríková <xkolar77@stud.fit.vutbr.cz>
2023-11-19 20:30:09 +0100

    code_generator functional basis

--- include/code_generator.h
+++ include/code_generator.h
@@ -3,7 +3,7 @@
  * @file code_generator.h
  * @brief Code generator header
  * @author Marie Kolarikova <xkolar77@stud.fit.vutbr.cz>
- * @date 12.11.2023
+ * @date 19.11.2023
  **/
 
 #ifndef CODE_GENERATOR_H
@@ -11,16 +11,193 @@
 
 #include "lexical_analyzer.h"
 
+/**
+ * Creates variable definition
+ * @param frame frame of declared variable
+ * @param varname name of declared variable
+ * @param id id of declared variable (default 0)
+ * 
+*/
 void code_generator_defvar(char *frame, char *varname, unsigned id);
 
+/**
+ * Checks if function needs new data frame
+ * @param name name of function
+ * @return bool
+*/
+bool code_generator_need_function_frame(char* name);
+
+/**
+ * Creates prolog of program (needed header)
+*/
 void code_generator_prolog();
 
-void code_generator_if_header(token_type_T operator, unsigned id);
+/**
+ * Creates header of if
+ * @pre On stack has to be value (bool) of condition
+ * @param id global unique id of if
+*/
+void code_generator_if_header(unsigned id);
 
+/**
+ * Creates else branch for if
+ * @param id global unique id of if
+*/
 void code_generator_if_else(unsigned id);
 
+/**
+ * Closes branches if and else
+ * @param id global unique id of if
+*/
 void code_generator_if_end(unsigned id);
 
+/**
+ * Token version of code_generator_var_assign
+ * @param token token of var identifier
+*/
+void code_generator_var_assign_token(token_T token);
+
+/**
+ * Sets value from stack to variable
+ * @param var name of variable
+*/
 void code_generator_var_assign(char* var);
+
+/**
+ * Token version of code_generator_var_declare
+ * @param token token of var identifier
+*/
+void code_generator_var_declare_token(token_T token);
+
+/**
+ * Declares and sets value from stack to variable (pop)
+ * @param var name of variable
+*/
+void code_generator_var_declare(char* variable);
+
+/**
+ * Creates eof label
+*/
+void code_generator_eof();
+
+/**
+ * Pushs value of token to stack
+ * @param token token to push (identifier/string/int/double)
+*/
+void code_generator_push(token_T token);
+
+/**
+ * Closes for loop
+ * @param id global unique id of for
+*/
+void code_generator_for_loop_end(unsigned id);
+
+/**
+ * Creates header of if for for loop
+ * @pre On stack has to be value (bool) of condition
+ * @param id global unique id of for
+*/
+void code_generator_for_loop_if(unsigned id);
+
+/**
+ * Creates label of for if calculation
+ * @param id global unique id of for
+*/
+void code_generator_for_label(unsigned id);
+
+/**
+ * Creates body of for
+ * @param id global unique id of for
+*/
+void code_generator_for_body(unsigned id);
+
+/**
+ * Prints value of token in assembler
+ * @param token token to print (identifier/string/int/double)
+*/
+void code_generator_print_value(token_T token);
+
+/**
+ * Pushs data frame
+*/
+void code_generator_pushframe();
+
+/**
+ * Creates data frame
+*/
+void code_generator_createframe();
+
+/**
+ * Pops data frame
+*/
+void code_generator_popframe();
+
+/**
+ * Creates operations on stack
+ * @param operator token type of operator
+ * @param is_int does operation in ints
+*/
+void code_generator_operations(token_type_T operator, bool is_int);
+
+/**
+ * Creates operation concat on stack
+*/
+void code_generator_concats();
+
+/**
+ * Clears stact
+*/
+void code_generator_clears();
+
+/**
+ * Token version of code_generator_function_call
+*/
+void code_generator_function_call_token(token_T token);
+
+/**
+ * Creates call of function
+ * @param name name of function
+*/
+void code_generator_function_call(char* name);
+
+/**
+ * Token version of code_generator_function_call_param_add
+ * @param token_name
+ * @param token_param_name
+ * @param token_value
+*/
+void code_generator_function_call_param_add_token(token_T token_name, token_T token_param_name, token_T token_value);
+
+/**
+ * Add parameter to future called function
+ * @pre Before adding the first parameter has to be called code_generator_createframe()
+ * @param name name of function
+ * @param param_name name of parameter
+ * @param value value of parameter
+*/
+void code_generator_function_call_param_add(char* name, char* param_name, token_T value);
+
+/**
+ * Token version of code_generator_function_label
+ * @param token
+*/
+void code_generator_function_label_token(token_T token);
+
+/**
+ * Creates label of function (declaration of header)
+ * @param name name of function
+*/
+void code_generator_function_label(char* name);
+
+/**
+ * Closes function
+*/
+void code_generator_function_end();
+
+/**
+ * Creates return from function
+ * @param token return value (identifier/string/int/double)
+*/
+void code_generator_return(token_T token);
 
 #endif
--- src/code_generator.c
+++ src/code_generator.c
@@ -3,7 +3,7 @@
  * @file code_generator.c
  * @brief Code generator
  * @author Marie Kolarikova <xkolar77@stud.fit.vutbr.cz>
- * @date 17.11.2023
+ * @date 19.11.2023
  **/
 
 #include "code_generator.h"
@@ -15,12 +15,36 @@ void code_generator_defvar(char *frame, char *varname, unsigned id){
     printf("\nDEFVAR %s@%s_%d\n",frame,varname, id);
 }
 
+bool code_generator_need_function_frame(char* name) {
+    const char* no_frame_funcions[] = {"readString", "readInt", "readDouble", "write", "Int2Double", "Double2Int", "length"};
+    const unsigned no_frame_funcions_count = 7;
+
+    for(unsigned i = 0; i < no_frame_funcions_count; i++){
+        if(strcmp(name, no_frame_funcions[i]) == 0) {
+            return false;  
+        }
+    }
+
+    return true;
+
+}
+
 void code_generator_prolog(){
 	printf(".IFJcode23\n");
 	code_generator_defvar("GF", "PARAM", 1);
 	code_generator_defvar("GF", "PARAM", 2);
 	code_generator_defvar("GF", "RESULT", 1);
+    code_generator_defvar("GF", "LENGTH", 1);
+    code_generator_defvar("GF", "READED", 1);
+    code_generator_defvar("GF", "READED", 2);
+    code_generator_defvar("GF", "READED", 3);
+    code_generator_createframe();
+    code_generator_pushframe();
 }
+
+/**
+ * If
+ */
 
 void code_generator_if_header(unsigned id){
     printf("\nPUSHS bool@true\n");
@@ -36,15 +60,27 @@ void code_generator_if_end(unsigned id){
 	printf("\nLABEL $$IF_END_%u\n", id);
 }
 
+/**
+ * Variable
+ */
+
+void code_generator_var_assign_token(token_T token){
+    code_generator_var_assign(token.value.string_val.str);
+}
+
 void code_generator_var_assign(char* var){
     
 	if(strcmp(var, "_") != 0){
-		printf("\nPOPS LF@%s\n", var);
+		printf("\nPOPS LF@%s_%d\n", var, 0);
 	} else{
         code_generator_defvar("LF","TMP",tmp_var_id);
 		printf("POPS LF@TMP_%u\n", tmp_var_id);
 		tmp_var_id++;
 	}
+}
+
+void code_generator_var_declare_token(token_T token){
+    code_generator_var_declare(token.value.string_val.str);
 }
 
 void code_generator_var_declare(char* variable){
@@ -55,6 +91,23 @@ void code_generator_var_declare(char* variable){
 void code_generator_eof(){
 	printf("\nLABEL $$EOF\n");
 }
+
+void code_generator_push(token_T token){
+    if(token.type != TOKEN_IDENTIFIER &&
+       token.type != TOKEN_INT &&
+       token.type != TOKEN_DBL &&
+       token.type != TOKEN_STRING) { 
+		return; 
+	}
+       
+    printf("\nPUSHS ");
+    code_generator_print_value(token);
+    printf("\n");
+}
+
+/**
+ * For loop
+ */
 
 void code_generator_for_loop_end(unsigned id){
     printf("\nJUMP $$FOR_%u\n",id);
@@ -75,29 +128,36 @@ void code_generator_for_body(unsigned id){
 	printf("\nLABEL $$FOR_BODY_%u\n", id);
 }
 
-void code_generator_print_value(token_T token){    
+/**
+ * Value
+ */
+
+void code_generator_print_value(token_T token){
 
     if(token.type == TOKEN_IDENTIFIER){
-        printf("\nLF@%s_%d",token.value.string_val.str, 0);
+        printf("LF@%s_%d",token.value.string_val.str, 0);
     } else if (token.type == TOKEN_INT) {
-			printf("\nint@%ld",token.value.int_val);
-		} else if (token.type == TOKEN_DBL) {
-			printf("\nfloat@%a",token.value.double_val);
-		} else if (token.type == TOKEN_STRING) {
-			printf("\nstring@");
-            int token_length = strlen(token.value.string_val.str);
+	    printf("int@%d",token.value.int_val);
+	} else if (token.type == TOKEN_DBL) {
+		printf("float@%a",token.value.double_val);
+	} else if (token.type == TOKEN_STRING) {
+		printf("string@");
+        int token_length = strlen(token.value.string_val.str);
 
-            for(int i = 0; i < token_length; i++){
-                char c = token.value.string_val.str[i];
-                if((c >= 0 && c <= 32) || c == '#' || c == '\\'){
-                    printf("\\%03d",c);
-                }
-                else{
-                    printf("%c", c);
-                }
+        for(int i = 0; i < token_length; i++){
+            char c = token.value.string_val.str[i];
+            if((c >= 0 && c <= 32) || c == '#' || c == '\\'){
+                printf("\\%03d",c);
+            } else{
+                printf("%c", c);
             }
-		}
+        }
+	}
 }
+
+/**
+ * Frame
+ */
 
 void code_generator_pushframe(){
     printf("\nPUSHFRAME\n");
@@ -111,37 +171,38 @@ void code_generator_createframe(){
     printf("\nCREATEFRAME\n");
 }
 
-void code_generator_push(token_T token){
-    if(token.type != TOKEN_IDENTIFIER &&
-       token.type != TOKEN_INT &&
-       token.type != TOKEN_DBL &&
-       token.type != TOKEN_STRING) { 
-		return; 
-	}
-       
-    printf("\nPUSHS ");
-    code_generator_print_value(token);
-    printf("\n");
-}
+/**
+ * Operations
+ */
 
-void code_generator_adds(){
-    printf("\nADDS\n");
-}
+void code_generator_operations(token_type_T operator, bool is_int){
 
-void code_generator_subs(){
-    printf("\nSUBS\n");
-}
-
-void code_generator_muls(){
-    printf("\nMULS\n");
-}
-
-void code_generator_divs(){
-    printf("\nDIVS\n");
-}
-
-void code_generator_idivs(){
-    printf("\nIDIVS\n");
+    if(operator == TOKEN_ADD) {
+        printf("\nADDS\n");
+    } else if (operator == TOKEN_SUB) {
+        printf("\nSUBS\n");
+    } else if (operator == TOKEN_MUL) {
+        printf("\nMULS\n");
+    } else if (operator == TOKEN_DIV && !is_int) {
+        printf("\nDIVS\n");
+    } else if (operator == TOKEN_DIV && is_int) {
+        printf("\nIDIVS\n");
+    } else if (operator == TOKEN_LT) {
+        printf("\nLTS\n");
+    } else if (operator == TOKEN_LEQ) {
+        printf("\nGTS\n");
+        printf("\nNOTS\n");
+    } else if (operator == TOKEN_GT) {
+        printf("\nGTS\n");
+    } else if (operator == TOKEN_GEQ) {
+        printf("\nLTS\n");
+        printf("\nNOTS\n");
+    } else if (operator == TOKEN_EQ) {
+        printf("\nEQS\n");
+    } else if (operator == TOKEN_NEQ) {
+        printf("\nEQS\n");
+        printf("\nNOTS\n");
+    }
 }
 
 void code_generator_concats(){
@@ -162,22 +223,76 @@ void code_generator_clears(){
     printf("\nCLEARS\n");
 }
 
+/**
+ * Function
+ */
+
+void code_generator_function_call_token(token_T token){
+    code_generator_function_call(token.value.string_val.str);
+}
+
 void code_generator_function_call(char* name){
 
-        if(strcmp(name,"readString") == 0){
-            printf("READ GF@READED_1 string");
-            printf("PUSHS GF@READED_1");
-        }
-        else if(strcmp(name,"readInt") == 0){
-            printf("READ GF@READED_2 int");
-            printf("PUSHS GF@READED_2");
-        }
-        else if((strcmp(name,"readDouble") == 0)){
-            printf("READ GF@READED_3 float");
-            printf("PUSHS GF@READED_3");
-        }
-        else if((strcmp(name,"write") == 0)){
-            
-           //TODO
-        }
-} 
+    if(strcmp(name,"readString") == 0){
+        printf("READ GF@READED_1 string\n");
+        printf("PUSHS GF@READED_1\n");
+    } else if(strcmp(name,"readInt") == 0){
+        printf("READ GF@READED_2 int\n");
+        printf("PUSHS GF@READED_2\n");
+    } else if((strcmp(name,"readDouble") == 0)){
+        printf("READ GF@READED_3 float\n");
+        printf("PUSHS GF@READED_3\n");
+    } else if (code_generator_need_function_frame(name)) {
+        printf("CALL $$FUNCTION_%s\n", name);
+    }
+}
+
+void code_generator_function_call_param_add_token(token_T token_name, token_T token_param_name, token_T token_value){
+    code_generator_function_call_param_add(token_name.value.string_val.str, token_param_name.value.string_val.str, token_value);
+}
+
+void code_generator_function_call_param_add(char* name, char* param_name, token_T token){
+
+    if(code_generator_need_function_frame(name)) {
+        code_generator_defvar("TF", param_name, 0);
+        printf("MOVE TF@%s_%d ",param_name, 0);
+        code_generator_print_value(token);
+        printf("\n");
+    }
+
+    if(strcmp(name,"write") == 0){
+        printf("WRITE ");
+        code_generator_print_value(token);
+        printf("\n");
+    } else if(strcmp(name,"Int2Double") == 0){
+        code_generator_push(token);
+        printf("INT2FLOATS\n");  
+    } else if((strcmp(name,"Double2Int") == 0)){
+        code_generator_push(token);
+        printf("FLOAT2INTS\n");
+    } else if((strcmp(name,"length") == 0)){
+        printf("STRLEN GF@LENGTH_1 ");
+        code_generator_print_value(token);
+        printf("\n");
+        printf("PUSHS GF@LENGTH_1\n");
+    }
+}
+
+void code_generator_function_label_token(token_T token){
+    code_generator_function_label(token.value.string_val.str);
+}
+
+void code_generator_function_label(char* name){
+    printf("\nLABEL $$FUNCTION_%s\n", name);
+    code_generator_pushframe();
+}
+
+void code_generator_function_end(){
+    code_generator_popframe();
+    printf("RETURN\n");
+}
+
+void code_generator_return(token_T token){
+    code_generator_push(token);
+    code_generator_function_end();
+}
