ad1008f22319764136f92cb90508eccd170526fe
Adnrew <ado.ponechal@gmail.com>
2023-12-04 18:59:39 +0100

    add implicit conversion prototype

--- src/expression.c
+++ src/expression.c
@@ -1187,11 +1187,26 @@ void convert_if_retypeable(symstack_data_t *operand1, symstack_data_t *operand2)
         if (operand1->is_literal && compare_operand_with_type(operand1, integer))
         {
             DEBUG_PRINT("retyping FIRST from int to double");
+
+            // DEFVAR       LF@first
+            // POPS         LF@first
+            code_generator_var_assign("_");
+            // DEFVAR       LF@second
+            // POPS         LF@second
+            code_generator_var_assign("_");
+
+            // int2float    LF@first
+            // PUSHS        LF@first
+            // PUSHS        LF@second
             operand1->expr_res.expr_type = double_;
         }
         else if (operand2->is_literal && compare_operand_with_type(operand2, integer))
         {
             DEBUG_PRINT("retyping SECOND from int to double");
+            // DEFVAR       TF@second
+            // POPS         TF@second
+            // int2float    TF@second
+            // PUSHS        TF@second
             operand2->expr_res.expr_type = double_;
         }
         else
